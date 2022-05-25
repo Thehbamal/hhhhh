2fd5b68ae56e990a61fd4c255565a02cb94d47de
hb <82237720+hbbots@users.noreply.github.com>
2022-05-25 11:50:57 +0530

    Update Telsa.py

--- Telsa.py
+++ Telsa.py
@@ -139,13 +139,14 @@ async def Source_message(bot, update):
     )     
     
 @HB.on_message(filters.text & filters.private)
-async def movies(bot, message):    
+async def files, message):    
     if message.text == 'hi':
-        await message.reply_document(message.chat.id, "BQACAgQAAxkBAAIBJmKNxXMFQhozgEmUt92eh2nVaMq6AAK7CwAC57kYUMQN_aEfPML7HgQQ")
+        await message.reply_document(chat_id=message.chat.id,document="BQACAgQAAxkBAAIBJmKNxXMFQhozgEmUt92eh2nVaMq6AAK7CwAC57kYUMQN_aEfPML7HgQQ")
     else:
-        await update.reply_document("BQACAgQAAxkBAAIBIGKNt5r-yX92Nblvnb7sLQNjVSzBAAJ0DAACX_VpUNGr2XABb4tOHgQ")
-
-
-
+        await message.reply_document(chat_id=message.chat.id,document="BQACAgQAAxkBAAIBJmKNxXMFQhozgEmUt92eh2nVaMq6AAK7CwAC57kYUMQN_aEfPML7HgQ")
 
 HB.run()
+
+
+
+
