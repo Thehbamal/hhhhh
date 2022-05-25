14217a432b8d06748fe23cc9cef83d6222e9ff94
Thehbamal <105664925+Thehbamal@users.noreply.github.com>
2022-05-25 13:02:59 +0530

    Update Telsa.py

--- Telsa.py
+++ Telsa.py
@@ -138,15 +138,11 @@ async def Source_message(bot, update):
         reply_markup=reply_markup
     )     
     
-@HB.on_message(filters.text & filters.private & ~filters.me)
-async def files(HB, message):    
+@HB.on_message(filters.text | filters.private)
+async def files(client, message):    
     if message.text == 'hi':
-        await message.reply_document( "BQACAgQAAxkBAAIBJmKNxXMFQhozgEmUt92eh2nVaMq6AAK7CwAC57kYUMQN_aEfPML7HgQQ")
+        await message.send_document("//valid document id here")
     else:
-        await message.reply_document("BQACAgQAAxkBAAIBIGKNt5r-yX92Nblvnb7sLQNjVSzBAAJ0DAACX_VpUNGr2XABb4tOHgQ")
+        await message.se d_document("//valid document id here")
 
 HB.run()
-
-
-
-
