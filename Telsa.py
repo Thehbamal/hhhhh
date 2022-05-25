553ced3f55866ab584f05ed26611e71d28914b2c
Thehbamal <105664925+Thehbamal@users.noreply.github.com>
2022-05-25 13:54:48 +0530

    Update Telsa.py

--- Telsa.py
+++ Telsa.py
@@ -138,9 +138,21 @@ async def Source_message(bot, update):
         reply_markup=reply_markup
     )     
     
-@HB.on_message(filters.command('audio'))
-def audio(bot,message):
-    bot.send_audio(message.chat.id, "")
-print("HB")  
+@HB.on_message(filters.text | filters.private)
+
+async def files(client, message):    
+
+    if message.text == 'hi':
+
+        await message.send_document(message.chat.id, "//valid document id here")
+
+    else:
+
+        await message.send_document(message.chat.id, "//valid document id here")
+
+HB.run()
+
+
+
 
 HB. run()
